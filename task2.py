5add3844f21123cf1b6749e9c8296765a34be2ae
Alexander Pavlov <zaaz.p@hotmail.com>
2019-09-19 17:31:33 +0900

    +tasks

---
+++ task2.py
@@ -0,0 +1,23 @@
+
+    ```python
+    def is_beauty(n):
+        ...
+    
+    ```
+   
+    ```python
+    def is_power_of_two(n):
+       ...
+ 
+    ```
+
+    ```python
+    def is_prime(n):
+        ...
+    
+    ```
+    ```python
+    def get_largest_perimiter(L):
+        ...
+    
+    ```
---
+++ task5.py
@@ -0,0 +1,5 @@
+def is_prime(n):
+	pass
+
+n = int(input())
+print(is_prime(n))
