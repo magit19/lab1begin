e18f488d2c04085fc285cab8d26ab192ca58a776
NikiforovI <NikiforovI@qweq.com>
2019-09-23 17:27:49 +0900

    3 сделана

--- task3.py
+++ task3.py
@@ -1,5 +1,16 @@
-def is_beautiful(n):
-	pass
+def digit_sum(n):
+	s = 0
+	while n > 0:
+		s = s + n % 10
+		n = n // 10
+	return s
 
+def is_beautiful(n):
+	if n % digit_sum(n) == 0:
+		return True
+	else:
+		return False
+
+print('Введите число')
 n = int(input())
 print(is_beautiful(n))
