ef929ca85f71ecf8e2a555ec9bf233d69d378f8f
Erken Neustroev <ea.neustroev@stud.s-vfu.ru>
2019-09-23 17:11:17 +0900

    наканессссстаааааааа

--- task2.py
+++ task2.py
@@ -1,6 +1,9 @@
 def digit_sum(n):
-    pass
-
+    s = 0
+    while n > 0:
+        s = s + n % 10
+        n = n // 10
+    return s    
 print('Введите число')
 n = int(input())
 print(digit_sum(n))
