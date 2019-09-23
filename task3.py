fac56fe017ee0e6dca420cc543e07453124c7a87
alekseev <wqdq@dsfsd.com>
2019-09-23 17:28:45 +0900

    task3

--- task3.py
+++ task3.py
@@ -1,5 +1,15 @@
+def digit_sum(n):
+    s = 0
+    while n > 0:
+        s = s + n % 10
+        n = n // 10
+    return s
+
 def is_beautiful(n):
-	pass
+	if n % digit_sum(n) == 0:
+		return True
+	else:
+		return False
 
 n = int(input())
 print(is_beautiful(n))
