e0c972c294c3e27cfcafb9c7352dacbae588170c
Denisovart <Denisov@nanoymouse.com>
2019-09-26 20:56:53 +0900

    sdelal task3

--- task3.py
+++ task3.py
@@ -1,5 +1,13 @@
 def is_beautiful(n):
-	pass
-
-n = int(input())
+	s = 0
+	a = n
+	while n > 0:
+		s = s + n%10
+		n = n//10 
+	if a%s == 0:
+		s = "True"
+	else:
+		s = "False"
+	return s
+n = int(input("Введите число: "))
 print(is_beautiful(n))
