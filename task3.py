3d0cfe04614ce0ef068b083e560d76cf5b190e95
Ilya <Ilya@fdgr.com>
2019-09-30 11:47:19 +0900

    sdelal3

--- task3.py
+++ task3.py
@@ -1,5 +1,12 @@
 def is_beautiful(n):
-	pass
+	s = 0
+	c = n
+	while n>0:
+		s = s + n%10
+		n = n // 10
+	if c%s == 0: s = ("True")
+	else: s = ("False")
+	return s
 
 n = int(input())
 print(is_beautiful(n))
