a60a64ecbd7843902a79c653f96e11f5a6600070
Alexey Koryakin <narutouzumaki@mail.konoha>
2019-10-06 15:02:53 +0900

    Sdelal #3,4,5

--- task3.py
+++ task3.py
@@ -1,22 +1,21 @@
 def is_beautiful(n):
 	s = 0
-	a = ('chislo')
+	
 	while n > 0:
 		s = s + n%10
 		n = n // 10
 
 
 
-	
+		
 
-	return s
-	bool
+	
 		if n%s == 0:
-			a = ('True')
+			s = True
 		else:
-			a = ('False')
-		return a
-print('Введи число')		
+			s = False
+		return s
+print('Введите число')		
 n = int(input())
 print(is_beautiful(n))
 
--- task4.py
+++ task4.py
@@ -1,5 +1,20 @@
 def is_power_of_two(n):
-	pass
+	s = 0
+	a = 1
+	while a != n:
+		a = a*2
+		if a > n:
+			break
+
+	if a == n:
+		s = True
+	else:
+		s = False
+	return s
+	
+	
+
+	
 
 n = int(input())
 print(is_power_of_two(n))
