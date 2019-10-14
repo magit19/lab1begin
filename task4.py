f6ba41133f35dbcc42f9cb1778909cbaf2343fa2
alexpotap1997 <mega.sentinel@gmail.com>
2019-10-14 16:51:05 +0900

    4 and 5

--- task4.py
+++ task4.py
@@ -1,5 +1,8 @@
 def is_power_of_two(n):
-	pass
-
+    if n == 1:
+        return True
+    if n & 1:
+        return False
+    return is_power_of_two(n >> 1)
 n = int(input())
 print(is_power_of_two(n))
--- task5.py
+++ task5.py
@@ -1,5 +1,11 @@
 def is_prime(n):
-	pass
-
+	if n > 1:
+		for i in range(2,n):
+			if (n % i) == 0:
+				return False
+		else:
+			return True
+	else:
+		return False
 n = int(input())
 print(is_prime(n))
