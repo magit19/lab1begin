200ab1d6a12e44fb7c9cd1e2f34b469d08e35374
AyzaBorisova <ayzaBorisova@Mail.ru>
2019-12-19 19:16:49 +0900

    1

--- task5.py
+++ task5.py
@@ -1,8 +1,8 @@
 def is_prime(n):
-	s = 2
-	if n == 1: return False
-	for a in range(2, n):
-		if n % s == 0: return False
-	else: return True
+	for i in range(2, (n-1)):
+		if n%i == 0: 
+			return False
+	else:
+	   return True
 n = int(input())
 print(is_prime(n))
