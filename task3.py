b4d987f2d7440ef8a2ba90bcf09e194cdd664bb5
EgorNogovitsin <EgorNogovit@sin.com>
2019-12-23 07:04:44 +0900

    3

---
+++ task3.py
@@ -0,0 +1,11 @@
+def is_beautiful(n):
+	s = 0
+	c = n
+	while n>0:
+		s = s + n%10
+		n = n // 10
+	if c%s == 0: s = ("True")
+	else: s = ("False")
+	return s
+n = int(input())
+print(is_beautiful(n))
