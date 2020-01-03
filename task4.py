fcc1bc40a2168c2bfb29f424194dc09f297d5639
Denisovart <Denisovart@nime.com>
2020-01-03 20:14:21 +0900

    Sdelal task 4

--- task4.py
+++ task4.py
@@ -1,9 +1,5 @@
 def is_power_of_two(n):
-	s = 0
-	if n & (n-1) == 0: #Нашел это решение в интернете, не очень понял что из себя предстовляет оператор "&"
-		s = "True"
-	else:
-		s = "False"
-	return s
+	a = n & (n-1) == 0
+	return a
 n = int(input("Введите число: "))
 print(is_power_of_two(n))
